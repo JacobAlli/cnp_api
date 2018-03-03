a5c6cdb045425872b9ef10d6c4bfad7669b5ba4e
Renee Ernst <reneeErnst322@gmail.com>
2018-03-02 19:23:16 -0600

    update GET routes

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,31 @@ router.get('/orders', function(req, res, next) {
   });
 });
 
+router.get('/products', function(req, res, next) {
+  db.Product.findAll().then((result) => {
+  	res.send(result);
+  });
+});
+
+router.get('/locations', function(req, res, next) {
+  db.Location.findAll().then((result) => {
+  	res.send(result);
+  });
+});
+
+router.get('/order_details', function(req, res, next) {
+  db.OrderDetail.findAll().then((result) => {
+  	res.send(result);
+  });
+});
+
+router.get('/users', function(req, res, next) {
+  db.User.findAll().then((result) => {
+  	res.send(result);
+  });
+});
+
+
 router.get('/', function(req, res, next) {
   res.send('hello');
 });
