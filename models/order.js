9840e5d063c4d7a5d7388e62e9e6fbb37020164e
Renee Ernst <reneeErnst322@gmail.com>
2018-03-11 11:36:30 -0500

    updated table strcutures

--- models/order.js
+++ models/order.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Order = sequelize.define('Order', {
-    user_Id: DataTypes.INTEGER
+    user_Id: DataTypes.INTEGER,
+    total: DataTypes.INTEGER,
+    description: DataTypes.STRING
   }, {});
   Order.associate = function(models) {
     // associations can be defined here
--- models/orderdetail.js
+++ models/orderdetail.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   var OrderDetail = sequelize.define('OrderDetail', {
     order_id: DataTypes.INTEGER,
-    product: DataTypes.STRING
+    user_id: DataTypes.INTEGER,
+    product_id: DataTypes.STRING
   }, {});
   OrderDetail.associate = function(models) {
     // associations can be defined here
